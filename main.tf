46138ac2f738fb2eb9d106ab2a081b0bc46be1bb
George <george.sashoff@reify.technology>
2021-12-29 19:12:43 +0200

    add setup for module lambda by aws

--- main.tf
+++ main.tf
@@ -26,12 +26,12 @@ module "s3_module" {
   lambda_role_arn = module.iam_module.lambda_role_arn
 }
 
-# module "lambda_s3" {
-#   source = "./lambdas/s3_lambda"
+module "lambda_s3" {
+  source = "./lambdas/s3_lambda"
 
-#   main_bucket_arn = module.s3_module.main_bucket_arn
-#   lambda_role_arn = module.iam_module.lambda_role_arn
-# }
+  main_bucket_arn = module.s3_module.main_bucket_arn
+  lambda_role_arn = module.iam_module.lambda_role_arn
+}
 
 # module "lambda_cw_alarm" {
 #   source = "./lambdas/cw_alarm_lambda"
@@ -39,23 +39,23 @@ module "s3_module" {
 #   lambda_role_arn = module.iam_module.lambda_role_arn
 # }
 
-module "lambda_module" {
-  source = "terraform-aws-modules/lambda/aws"
+# module "lambda_module" {
+#   source = "terraform-aws-modules/lambda/aws"
 
-  function_name = "reify-s3-lambda"
-  description   = "My awesome lambda function"
-  handler       = "index.lambda_handler"
-  runtime       = "python3.8"
+#   function_name = "reify-s3-lambda"
+#   description   = "My awesome lambda function"
+#   handler       = "index.lambda_handler"
+#   runtime       = "python3.8"
 
-  source_path = "./lambdas/src/s3_lambda"
+#   source_path = "./lambdas/src/s3_lambda"
 
-  environment_variables = {
-    S3_BUCKET_ARN = module.iam_module.lambda_role_arn
-    IAM_ROLE_ARN  = module.s3_module.main_bucket_arn
+#   environment_variables = {
+#     S3_BUCKET_ARN = module.iam_module.lambda_role_arn
+#     IAM_ROLE_ARN  = module.s3_module.main_bucket_arn
 
-  }
+#   }
 
-  tags = {
-    Name = "reify-s3-lambda"
-  }
-}
+#   tags = {
+#     Name = "reify-s3-lambda"
+#   }
+# }
